3c7f29c976b6609ee71e8bafc15974d70a8dfe7b
Moritz Lang <hi@slashmo.codes>
2022-03-17 13:15:30 +0100

    [CI] Run on host instead of Docker

--- Examples/Onboarding/Package.swift
+++ Examples/Onboarding/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .executable(name: "onboarding", targets: ["Onboarding"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/slashmo/opentelemetry-swift.git", .branch("main")),
+        .package(name: "opentelemetry-swift", path: "../../"),
     ],
     targets: [
         .target(name: "Onboarding", dependencies: [
